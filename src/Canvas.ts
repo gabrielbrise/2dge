bb18c8387f831490152d7e61aae107475bd18b00
Brise <gabriel_ns@live.com>
2021-05-29 19:36:15 -0300

    refactor: Canvas interface

--- src/Canvas.ts
+++ src/Canvas.ts
@@ -1,6 +1,6 @@
 import Keyboard from "./Keyboard";
 
-export default class Canvas {
+export interface ICanvas {
   height: number;
   width: number;
   id: string;
@@ -9,6 +9,11 @@ export default class Canvas {
   keyboard: any;
   timestamp: DOMHighResTimeStamp;
   objects: any[];
+}
+
+interface Canvas extends ICanvas {}
+
+class Canvas {
   constructor(width: number = 640, height: number = 480, objects: any[] = []) {
     this.height = height;
     this.width = width;
@@ -32,41 +37,6 @@ export default class Canvas {
     this.objects.forEach((object) => {
       object.draw();
     });
-    //   ctx.globalCompositeOperation = "destination-over";
-    //   ctx.clearRect(0, 0, 300, 300); // clear canvas
-
-    //   ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
-    //   ctx.strokeStyle = "rgba(0, 153, 255, 0.4)";
-    //   ctx.save();
-    //   ctx.translate(150, 150);
-
-    //   // Earth
-    //   var time = new Date();
-    //   ctx.rotate(
-    //     ((2 * Math.PI) / 60) * time.getSeconds() +
-    //       ((2 * Math.PI) / 60000) * time.getMilliseconds()
-    //   );
-    //   ctx.translate(105, 0);
-    //   ctx.fillRect(0, -12, 40, 24); // Shadow
-    //   ctx.drawImage(earth, -12, -12);
-
-    //   // Moon
-    //   ctx.save();
-    //   ctx.rotate(
-    //     ((2 * Math.PI) / 6) * time.getSeconds() +
-    //       ((2 * Math.PI) / 6000) * time.getMilliseconds()
-    //   );
-    //   ctx.translate(0, 28.5);
-    //   ctx.drawImage(moon, -3.5, -3.5);
-    //   ctx.restore();
-
-    //   ctx.restore();
-
-    //   ctx.beginPath();
-    //   ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-    //   ctx.stroke();
-
-    //   ctx.drawImage(sun, 0, 0, 300, 300);
 
     window.requestAnimationFrame((timestamp) => this.draw(timestamp));
   }
@@ -83,3 +53,5 @@ export default class Canvas {
     this.objects.push(newObject);
   }
 }
+
+export default Canvas;
